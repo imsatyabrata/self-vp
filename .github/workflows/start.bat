96161aa759f7e0d9327187874a59c698d5be5e92
imsatyabrata <76495730+imsatyabrata@users.noreply.github.com>
2021-12-25 02:07:28 +0530

    Update start.bat

--- .github/workflows/start.bat
+++ .github/workflows/start.bat
@@ -17,6 +17,6 @@ echo IP:
 tasklist | find /i "ngrok.exe" >Nul && curl -s localhost:4040/api/tunnels | jq -r .tunnels[0].public_url || echo "Cannot get a troop tunnel, make sure ngrok_auth_token is correct in Settings> Secrets> Repository Secret.Maybe your previous VM is still running:
 https://dashboard.ngrok.com/status/tunnels "
 echo Username: administrator
-echo Password: @Logmein!$
+echo Password: @GetdDrive!$
 echo Please log in to your RDP!
 ping -n 10 127.0.0.1 >nul
